026a0956c50c71d6db62cfb504d16a64ba6031f0
christojohnsonv <christojohnsonv@gmail.com>
2023-10-29 15:25:18 +0530

    Login Page and view added.

--- base/views.py
+++ base/views.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.shortcuts import render
+from django.urls import reverse
+from django.views import View
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+    redirect_authenticated_user = True
+
+    def get_success_url(self) -> str:
+        return reverse('dashboard')
+
+
+@method_decorator(login_required, name="dispatch")
+class DashboardView(TemplateView):
+    template_name = "layout.html"
