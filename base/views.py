f2e3ca9e92c3ec43d74d5e0097fc11d7457fce0b
christojohnsonv <christojohnsonv@gmail.com>
2023-10-29 15:34:38 +0530

    Flake issues fixed.

--- base/views.py
+++ base/views.py
@@ -1,9 +1,7 @@
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from django.shortcuts import render
 from django.urls import reverse
-from django.views import View
 from django.views.generic.base import TemplateView
 
 
